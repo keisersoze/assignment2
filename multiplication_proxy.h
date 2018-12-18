4fd901784ffd3525cd858bb987c3341a80aa2b1b
Unknown <filippomaganza@gmail.com>
2018-12-18 21:39:15 +0100

    suummmmmm

--- multiplication_proxy.h
+++ multiplication_proxy.h
@@ -103,18 +103,27 @@ public:
 
 template<class T,class U, class dec, class dec2>
 matrix<decltype(T()+U())> operator+ (const matrix_ref<T,dec>& m, const matrix_ref<U,dec2>& m2){
-    if constexpr (m.is_ct() && m2.is_ct())
-        if constexpr (m.get_ct_height() != m2.get_ct_height() || m.get_ct_width() != m2.get_width())
+    if constexpr (m.is_ct() && m2.is_ct()) {
+        if constexpr (m.get_ct_height() != m2.get_ct_height() || m.get_ct_width() != m2.get_ct_width())
             throw "Size mismatch";
-    else if (m.get_width() != m2.get_width() || m.get_height() != m2.get_height())
-        throw "Size mismatch";
-    matrix<decltype(U()+T())> result(m.get_height(),m.get_width());
-    for (int i = 0; i < m.get_height(); ++i) {
-        for (int j = 0; j < m.get_width() ; ++j) {
-            result(i,j)=m(i,j)+m2(i,j);
+        matrix<decltype(U()+T()), m.get_ct_height(), m.get_ct_width() > result;
+        for (int i = 0; i < m.get_ct_height() ; ++i) {
+            for (int j = 0; j < m.get_ct_width() ; ++j) {
+                result(i,j)=m(i,j)+m2(i,j);
+            }
         }
+        return result;
+    }else {
+        if (m.get_width() != m2.get_width() || m.get_height() != m2.get_height())
+            throw "Size mismatch";
+        matrix<decltype(U()+T())> result (m.get_height(),m.get_width());
+        for (int i = 0; i < m.get_height(); ++i) {
+            for (int j = 0; j < m.get_width() ; ++j) {
+                result(i,j)=m(i,j)+m2(i,j);
+            }
+        }
+        return result;
     }
-    return result;
 }
 
 
